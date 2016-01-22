1b92dca0959768079878c5edfc8c4a4d0def8258
Seunghoon Ko <imskojs@gmail.com>
2016-01-22 11:10:08 +0900

    test

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -33,6 +33,17 @@ module.exports.bootstrap = function(cb) {
 
   //   });
   // });
+  User.find({})
+    .then((users) => {
+      sails.log("-----------  users  -------------");
+      sails.log(users);
+      cb();
+    })
+    .catch((err) => {
+      sails.log("-----------  err  -------------");
+      sails.log(err);
+      cb();
+    });
 
 
 
@@ -77,247 +88,247 @@ module.exports.bootstrap = function(cb) {
   //  DANGER !!!!
   //====================================================
 
-  var Converter = require("csvtojson").Converter;
-  var converterField = new Converter({});
-  var converterClub = new Converter({});
-  var converterBall = new Converter({});
-  var Promise = require('bluebird');
-  var _ = require('lodash');
+  // var Converter = require("csvtojson").Converter;
+  // var converterField = new Converter({});
+  // var converterClub = new Converter({});
+  // var converterBall = new Converter({});
+  // var Promise = require('bluebird');
+  // var _ = require('lodash');
 
 
-  var pendingField = Promise.pending();
-  converterField.on("end_parsed", function(jsonArray) {
-    pendingField.resolve(jsonArray);
-  });
-  var pendingClub = Promise.pending();
-  converterClub.on("end_parsed", function(jsonArray) {
-    pendingClub.resolve(jsonArray);
-  });
-  var pendingBall = Promise.pending();
-  converterBall.on("end_parsed", function(jsonArray) {
-    pendingBall.resolve(jsonArray);
-  });
-  require("fs").createReadStream(__dirname + "/../assets/csvfiles/field.csv").pipe(converterField);
-  require("fs").createReadStream(__dirname + "/../assets/csvfiles/club.csv").pipe(converterClub);
-  require("fs").createReadStream(__dirname + "/../assets/csvfiles/ball.csv").pipe(converterBall);
+  // var pendingField = Promise.pending();
+  // converterField.on("end_parsed", function(jsonArray) {
+  //   pendingField.resolve(jsonArray);
+  // });
+  // var pendingClub = Promise.pending();
+  // converterClub.on("end_parsed", function(jsonArray) {
+  //   pendingClub.resolve(jsonArray);
+  // });
+  // var pendingBall = Promise.pending();
+  // converterBall.on("end_parsed", function(jsonArray) {
+  //   pendingBall.resolve(jsonArray);
+  // });
+  // require("fs").createReadStream(__dirname + "/../assets/csvfiles/field.csv").pipe(converterField);
+  // require("fs").createReadStream(__dirname + "/../assets/csvfiles/club.csv").pipe(converterClub);
+  // require("fs").createReadStream(__dirname + "/../assets/csvfiles/ball.csv").pipe(converterBall);
 
-  var globalAdminId;
-  Promise.all([pendingField.promise, pendingClub.promise, pendingBall.promise])
-    .then(function(array) {
-      var fields = array[0];
-      var clubs = array[1];
-      var balls = array[2];
-      var userPromise = User.findOne({
-        username: 'admin'
-      });
-      return [userPromise, fields, clubs, balls];
-    })
-    .spread(function(admin, fields, clubs, balls) {
-      globalAdminId = admin.id;
-      var fieldsData = _.map(fields, function(field) {
-        field.owner = admin.id;
-        field.createdBy = admin.id;
-        field.updatedBy = admin.id;
-        return field;
-      });
-      var clubsData = _.map(clubs, function(club) {
-        club.owner = admin.id;
-        club.createdBy = admin.id;
-        club.updatedBy = admin.id;
-        return club;
-      });
-      var ballsData = _.map(balls, function(ball) {
-        ball.owner = admin.id;
-        ball.createdBy = admin.id;
-        ball.updatedBy = admin.id;
-        return ball;
-      });
-      var createFieldPromise;
-      var createClubsPromise;
-      var createBallsPromise;
-      createFieldPromise = Product.create(fieldsData);
-      createClubsPromise = Product.create(clubsData);
-      createBallsPromise = Product.create(ballsData);
-      return [createFieldPromise, createClubsPromise, createBallsPromise];
-    })
-    .spread(function(createdFields, createdClubs, createdBalls) {
-      var converterQuestion = new Converter({});
-      var pendingQuestion = Promise.pending();
-      converterQuestion.on("end_parsed", function(jsonArray) {
-        pendingQuestion.resolve(jsonArray);
-      });
-      require("fs").createReadStream(__dirname + "/../assets/csvfiles/question.csv")
-        .pipe(converterQuestion);
+  // var globalAdminId;
+  // Promise.all([pendingField.promise, pendingClub.promise, pendingBall.promise])
+  //   .then(function(array) {
+  //     var fields = array[0];
+  //     var clubs = array[1];
+  //     var balls = array[2];
+  //     var userPromise = User.findOne({
+  //       username: 'admin'
+  //     });
+  //     return [userPromise, fields, clubs, balls];
+  //   })
+  //   .spread(function(admin, fields, clubs, balls) {
+  //     globalAdminId = admin.id;
+  //     var fieldsData = _.map(fields, function(field) {
+  //       field.owner = admin.id;
+  //       field.createdBy = admin.id;
+  //       field.updatedBy = admin.id;
+  //       return field;
+  //     });
+  //     var clubsData = _.map(clubs, function(club) {
+  //       club.owner = admin.id;
+  //       club.createdBy = admin.id;
+  //       club.updatedBy = admin.id;
+  //       return club;
+  //     });
+  //     var ballsData = _.map(balls, function(ball) {
+  //       ball.owner = admin.id;
+  //       ball.createdBy = admin.id;
+  //       ball.updatedBy = admin.id;
+  //       return ball;
+  //     });
+  //     var createFieldPromise;
+  //     var createClubsPromise;
+  //     var createBallsPromise;
+  //     createFieldPromise = Product.create(fieldsData);
+  //     createClubsPromise = Product.create(clubsData);
+  //     createBallsPromise = Product.create(ballsData);
+  //     return [createFieldPromise, createClubsPromise, createBallsPromise];
+  //   })
+  //   .spread(function(createdFields, createdClubs, createdBalls) {
+  //     var converterQuestion = new Converter({});
+  //     var pendingQuestion = Promise.pending();
+  //     converterQuestion.on("end_parsed", function(jsonArray) {
+  //       pendingQuestion.resolve(jsonArray);
+  //     });
+  //     require("fs").createReadStream(__dirname + "/../assets/csvfiles/question.csv")
+  //       .pipe(converterQuestion);
 
-      return [createdFields, createdClubs, createdBalls, pendingQuestion.promise];
-    })
-    .spread((createdFields, createdClubs, createdBalls, questions) => {
-      let preFieldQuestions = _.where(questions, {
-        type: 'FIELD'
-      });
-      let preClubQuestions = _.where(questions, {
-        type: 'CLUB'
-      });
-      let preBallQuestions = _.where(questions, {
-        type: 'BALL'
-      });
-      var fieldQuestions = properlyFormatQuestions(preFieldQuestions);
-      var clubQuestions = properlyFormatQuestions(preClubQuestions);
-      var ballQuestions = properlyFormatQuestions(preBallQuestions);
-
-
-      var ballCompletePromises = _.map(createdBalls, function(ballProduct) {
-        var ballQuestionnairePromises = _.map([0, 1, 2, 3, 4], function(i) {
-          return Questionnaire.create({
-              position: i,
-              type: 'BALL',
-              category: 'SURVEY',
-              product: ballProduct.id,
-              owner: globalAdminId,
-              createdBy: globalAdminId,
-              updatedBy: globalAdminId
-            })
-            .then((createdQuestionnaire) => {
-              var subBallQuestions = _.where(ballQuestions, {
-                position: i
-              });
-              var questionPromise = _.map(subBallQuestions, function(subBallQuestion) {
-                return Question.create({
-                  position: i,
-                  type: 'BALL',
-                  title: subBallQuestion.title,
-                  description: subBallQuestion.description || null,
-                  options: subBallQuestion.options,
-                  questionnaire: createdQuestionnaire.id,
-                  owner: globalAdminId,
-                  createdBy: globalAdminId,
-                  updatedBy: globalAdminId
-                });
-              });
-              return Promise.all(questionPromise);
-            });
-        });
-        return Promise.all(ballQuestionnairePromises);
-      });
+  //     return [createdFields, createdClubs, createdBalls, pendingQuestion.promise];
+  //   })
+  //   .spread((createdFields, createdClubs, createdBalls, questions) => {
+  //     let preFieldQuestions = _.where(questions, {
+  //       type: 'FIELD'
+  //     });
+  //     let preClubQuestions = _.where(questions, {
+  //       type: 'CLUB'
+  //     });
+  //     let preBallQuestions = _.where(questions, {
+  //       type: 'BALL'
+  //     });
+  //     var fieldQuestions = properlyFormatQuestions(preFieldQuestions);
+  //     var clubQuestions = properlyFormatQuestions(preClubQuestions);
+  //     var ballQuestions = properlyFormatQuestions(preBallQuestions);
 
 
-      var clubCompletePromises = _.map(createdClubs, function(clubProduct) {
-        var clubQuestionnairePromises = _.map([0, 1, 2, 3, 4], function(i) {
-          return Questionnaire.create({
-              position: i,
-              type: 'CLUB',
-              category: 'SURVEY',
-              product: clubProduct.id,
-              owner: globalAdminId,
-              createdBy: globalAdminId,
-              updatedBy: globalAdminId
-            })
-            .then((createdQuestionnaire) => {
-              var subClubQuestions = _.where(clubQuestions, {
-                position: i
-              });
-              var questionPromise = _.map(subClubQuestions, function(subClubQuestion) {
-                return Question.create({
-                  position: i,
-                  type: 'CLUB',
-                  title: subClubQuestion.title,
-                  description: subClubQuestion.description || null,
-                  options: subClubQuestion.options,
-                  questionnaire: createdQuestionnaire.id,
-                  owner: globalAdminId,
-                  createdBy: globalAdminId,
-                  updatedBy: globalAdminId
-                });
-              });
-              return Promise.all(questionPromise);
-            });
-        });
-        return Promise.all(clubQuestionnairePromises);
-      });
+  //     var ballCompletePromises = _.map(createdBalls, function(ballProduct) {
+  //       var ballQuestionnairePromises = _.map([0, 1, 2, 3, 4], function(i) {
+  //         return Questionnaire.create({
+  //             position: i,
+  //             type: 'BALL',
+  //             category: 'SURVEY',
+  //             product: ballProduct.id,
+  //             owner: globalAdminId,
+  //             createdBy: globalAdminId,
+  //             updatedBy: globalAdminId
+  //           })
+  //           .then((createdQuestionnaire) => {
+  //             var subBallQuestions = _.where(ballQuestions, {
+  //               position: i
+  //             });
+  //             var questionPromise = _.map(subBallQuestions, function(subBallQuestion) {
+  //               return Question.create({
+  //                 position: i,
+  //                 type: 'BALL',
+  //                 title: subBallQuestion.title,
+  //                 description: subBallQuestion.description || null,
+  //                 options: subBallQuestion.options,
+  //                 questionnaire: createdQuestionnaire.id,
+  //                 owner: globalAdminId,
+  //                 createdBy: globalAdminId,
+  //                 updatedBy: globalAdminId
+  //               });
+  //             });
+  //             return Promise.all(questionPromise);
+  //           });
+  //       });
+  //       return Promise.all(ballQuestionnairePromises);
+  //     });
 
 
-      var fieldCompletePromises = _.map(createdFields, function(fieldProduct) {
-        var fieldQuestionnairePromises = _.map([0, 1, 2, 3, 4], function(i) {
-          return Questionnaire.create({
-              position: i,
-              type: 'FIELD',
-              category: 'SURVEY',
-              product: fieldProduct.id,
-              owner: globalAdminId,
-              createdBy: globalAdminId,
-              updatedBy: globalAdminId
-            })
-            .then((createdQuestionnaire) => {
-              var subFieldQuestions = _.where(fieldQuestions, {
-                position: i
-              });
-              var questionPromise = _.map(subFieldQuestions, function(subFieldQuestion) {
-                return Question.create({
-                  position: i,
-                  type: 'FIELD',
-                  title: subFieldQuestion.title,
-                  description: subFieldQuestion.description || null,
-                  options: subFieldQuestion.options,
-                  questionnaire: createdQuestionnaire.id,
-                  owner: globalAdminId,
-                  createdBy: globalAdminId,
-                  updatedBy: globalAdminId
-                });
-              });
-              return Promise.all(questionPromise);
-            });
-        });
-        return Promise.all(fieldQuestionnairePromises);
-      });
+  //     var clubCompletePromises = _.map(createdClubs, function(clubProduct) {
+  //       var clubQuestionnairePromises = _.map([0, 1, 2, 3, 4], function(i) {
+  //         return Questionnaire.create({
+  //             position: i,
+  //             type: 'CLUB',
+  //             category: 'SURVEY',
+  //             product: clubProduct.id,
+  //             owner: globalAdminId,
+  //             createdBy: globalAdminId,
+  //             updatedBy: globalAdminId
+  //           })
+  //           .then((createdQuestionnaire) => {
+  //             var subClubQuestions = _.where(clubQuestions, {
+  //               position: i
+  //             });
+  //             var questionPromise = _.map(subClubQuestions, function(subClubQuestion) {
+  //               return Question.create({
+  //                 position: i,
+  //                 type: 'CLUB',
+  //                 title: subClubQuestion.title,
+  //                 description: subClubQuestion.description || null,
+  //                 options: subClubQuestion.options,
+  //                 questionnaire: createdQuestionnaire.id,
+  //                 owner: globalAdminId,
+  //                 createdBy: globalAdminId,
+  //                 updatedBy: globalAdminId
+  //               });
+  //             });
+  //             return Promise.all(questionPromise);
+  //           });
+  //       });
+  //       return Promise.all(clubQuestionnairePromises);
+  //     });
 
 
-      return [
-        Promise.all(fieldCompletePromises),
-        Promise.all(clubCompletePromises),
-        Promise.all(ballCompletePromises)
-      ];
+  //     var fieldCompletePromises = _.map(createdFields, function(fieldProduct) {
+  //       var fieldQuestionnairePromises = _.map([0, 1, 2, 3, 4], function(i) {
+  //         return Questionnaire.create({
+  //             position: i,
+  //             type: 'FIELD',
+  //             category: 'SURVEY',
+  //             product: fieldProduct.id,
+  //             owner: globalAdminId,
+  //             createdBy: globalAdminId,
+  //             updatedBy: globalAdminId
+  //           })
+  //           .then((createdQuestionnaire) => {
+  //             var subFieldQuestions = _.where(fieldQuestions, {
+  //               position: i
+  //             });
+  //             var questionPromise = _.map(subFieldQuestions, function(subFieldQuestion) {
+  //               return Question.create({
+  //                 position: i,
+  //                 type: 'FIELD',
+  //                 title: subFieldQuestion.title,
+  //                 description: subFieldQuestion.description || null,
+  //                 options: subFieldQuestion.options,
+  //                 questionnaire: createdQuestionnaire.id,
+  //                 owner: globalAdminId,
+  //                 createdBy: globalAdminId,
+  //                 updatedBy: globalAdminId
+  //               });
+  //             });
+  //             return Promise.all(questionPromise);
+  //           });
+  //       });
+  //       return Promise.all(fieldQuestionnairePromises);
+  //     });
 
-    })
-    .spread((fieldQuestionnaires, clubQuestionnaires, ballQuestionnaires) => {
-      sails.log("-----------  fieldQuestionnaires && fieldQuestionnaires[0]  -------------");
-      sails.log(fieldQuestionnaires && fieldQuestionnaires[0]);
-      sails.log("-----------  clubQuestionnaires && clubQuestionnaires[0]  -------------");
-      sails.log(clubQuestionnaires && clubQuestionnaires[0]);
-      sails.log("-----------  ballQuestionnaires && ballQuestionnaires[0]  -------------");
-      sails.log(ballQuestionnaires && ballQuestionnaires[0]);
-    })
-    .then(function() {
-      cb();
-    })
-    .catch(function(err) {
-      sails.log("-----------  err  -------------");
-      sails.log(err);
-    });
+
+  //     return [
+  //       Promise.all(fieldCompletePromises),
+  //       Promise.all(clubCompletePromises),
+  //       Promise.all(ballCompletePromises)
+  //     ];
+
+  //   })
+  //   .spread((fieldQuestionnaires, clubQuestionnaires, ballQuestionnaires) => {
+  //     sails.log("-----------  fieldQuestionnaires && fieldQuestionnaires[0]  -------------");
+  //     sails.log(fieldQuestionnaires && fieldQuestionnaires[0]);
+  //     sails.log("-----------  clubQuestionnaires && clubQuestionnaires[0]  -------------");
+  //     sails.log(clubQuestionnaires && clubQuestionnaires[0]);
+  //     sails.log("-----------  ballQuestionnaires && ballQuestionnaires[0]  -------------");
+  //     sails.log(ballQuestionnaires && ballQuestionnaires[0]);
+  //   })
+  //   .then(function() {
+  //     cb();
+  //   })
+  //   .catch(function(err) {
+  //     sails.log("-----------  err  -------------");
+  //     sails.log(err);
+  //   });
 
 
 
-  function properlyFormatQuestions(questions) {
-    var properlyFormattedQuestions = _.map(questions, function(question) {
-      var temp = {};
-      temp.type = question.type;
-      temp.position = question.position;
-      temp.title = question.title;
-      temp.options = [];
-      if (question.description) {
-        temp.description = question.description;
-      }
-      for (let i = 0; i < 5; i++) {
-        if (question['label' + i]) {
-          temp.options.push({
-            label: question['label' + i],
-            score: question['score' + i]
-          });
-        }
-      }
-      return temp;
-    });
-    return properlyFormattedQuestions;
-  }
+  // function properlyFormatQuestions(questions) {
+  //   var properlyFormattedQuestions = _.map(questions, function(question) {
+  //     var temp = {};
+  //     temp.type = question.type;
+  //     temp.position = question.position;
+  //     temp.title = question.title;
+  //     temp.options = [];
+  //     if (question.description) {
+  //       temp.description = question.description;
+  //     }
+  //     for (let i = 0; i < 5; i++) {
+  //       if (question['label' + i]) {
+  //         temp.options.push({
+  //           label: question['label' + i],
+  //           score: question['score' + i]
+  //         });
+  //       }
+  //     }
+  //     return temp;
+  //   });
+  //   return properlyFormattedQuestions;
+  // }
 
 
 
