0286b4e65574fdb2675ae1618d423d7cb34b5790
Seunghoon Ko <imskojs@gmail.com>
2016-03-25 13:58:37 +0900

    update question csvfile 비거리

--- CreateProductLogic.js
+++ CreateProductLogic.js
@@ -1,15 +1,15 @@
-var product = {
-  type: 'CLUB',
-  position: 0,
-  title: '비거리',
-  label0: '10% 이상 증가',
-  score0: 5,
-  label1: '10% 증가',
-  score1: 4,
-  label2: '5% 증가',
-  score2: 3,
-  label3: '변화없음',
-  score3: 2,
-  label4: '이전만 못함',
-  score4: 1
-};
+// var product = {
+//   type: 'CLUB',
+//   position: 0,
+//   title: '비거리',
+//   label0: '10% 이상 증가',
+//   score0: 5,
+//   label1: '10% 증가',
+//   score1: 4,
+//   label2: '5% 증가',
+//   score2: 3,
+//   label3: '변화없음',
+//   score3: 2,
+//   label4: '이전만 못함',
+//   score4: 1
+// };
